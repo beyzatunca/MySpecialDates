86f5e06d90f580fe66660f0e78a5f30a123e7eac
Beyza Erdemli <beyzaerdemli@mac.speedport.ip>
2025-08-07 21:04:21 +0200

    Add Xcode project structure with SwiftUI iOS app

---
+++ MySpecialDates/MySpecialDates/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MySpecialDates
+//
+//  Created by Beyza Erdemli on 07.08.25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Mother Fucker, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MySpecialDates/MySpecialDates/MySpecialDatesApp.swift
@@ -0,0 +1,17 @@
+//
+//  MySpecialDatesApp.swift
+//  MySpecialDates
+//
+//  Created by Beyza Erdemli on 07.08.25.
+//
+
+import SwiftUI
+
+@main
+struct MySpecialDatesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
